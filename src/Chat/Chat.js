1b49cfa8f44a18bb6fedf8ac0f51d9bc2ed9553a
judesama <judeagagad@gmail.com>
2019-07-23 21:54:44 -0800

    update layout

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -104,44 +104,44 @@ export default class Chat extends React.Component{
     render(){
       const { TextArea } = Input;
       return (
-        <div style={{padding: '15px'}}>
-          <Row type="flex" justify="space-around" align="middle">
-              <Col className="chatsContainer" span={18} style={{}}>
+        <div>
+              <div className="msgContainer">
                 <List
                     itemLayout="horizontal"
                     dataSource={data}
                     renderItem={item => (
-                      <List.Item actions={[<a>Delete</a>]}>
+                      <List.Item style={{paddingLeft: '15px'}} actions={[<a>Delete</a>]}>
                         <List.Item.Meta
                           title={<a href="https://ant.design">{item.title}</a>}
                           description={item.description}
                         />
                       </List.Item>
                     )}
-                  />
+                />
 
-              </Col>
+              </div>
+
 
               
             
-          </Row>
+       
 
-          <Row type="flex" justify="space-around" align="middle" style={{paddingTop: '25px'}}>
-              <Col span={18}>
-                <Row type="flex">
+        
+           
+                {/* <Row type="flex">
                   <Col span={22} >
                     <TextArea Placeholder="Message"/>
                   </Col>
 
-                  <Col span={1} style={{marginLeft: '5px' }}>
+                
                   <Button type="primary" style={{width:'100px'}}>
                     Send
                   </Button>
-                  </Col>
-                </Row>
-              </Col>
+                  </Col> */}
+               
              
-          </Row>
+             
+        
 
         </div>
       )
